440171afe4db3243ca98afae16d97d78d2426b53
Aaron Du <151097536+Duaarondu@users.noreply.github.com>
2025-12-09 12:12:23 -0500

    Added SQL database to the project

--- src/Connecter.java
+++
@@ -1,9 +0,0 @@
-
-import java.sql.Connection;
-
-public class Connecter {
-    public static void main(String[] args) {
-        Database.connect();
-    }
-
-}
--- src/Database.java
+++ src/Database.java
@@ -1,8 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
-
 class Database {
     public static Connection connection;
 
